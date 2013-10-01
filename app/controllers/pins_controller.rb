f4f6489f14eaa1b937e39e65c0585ba5e47d7578
Daniel Alessandro <dalessandro5138@gmail.com>
2013-10-01 18:57:37 -0400

    Change DB Configs

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -69,7 +69,7 @@ class PinsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
-      @pin = Pin.find(params[:id])
+      @pin = current_user.pins.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
